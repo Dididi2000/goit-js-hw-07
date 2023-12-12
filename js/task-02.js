dad8c20da85a9783adc0a847d0fcc87147fc4133
Dididi2000 <142427785+Dididi2000@users.noreply.github.com>
2023-12-13 00:23:28 +0300

    task-2

--- js/task-02.js
+++ js/task-02.js
@@ -27,3 +27,23 @@ const images = [
     alt: 'Zebras on Zebra',
   },
 ];
+
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML(
+  'afterbegin',
+  `<li><img src="https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="White and Black Long Fur Cat" />
+  </li>
+  <li><img src="https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" />
+  </li>
+  <li><img src="https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="Group of Horses Running" />
+  </li>
+  <li><img src="https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Elephant Beside on Baby Elephant" />
+  </li>
+  <li><img src="https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Blue Geeen and Orange Parrot" />
+  </li>
+  <li><img src="https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="Zebras on Zebra" />
+  </li>
+  `
+);
+
